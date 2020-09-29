0164c490fe532e7c95fe70a11f5cff6d592eb2ee
Sam Passarelli <passarelli.sam@gmail.com>
2020-09-29 20:31:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.select do |element|
-    
+    element.length > 4 
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
 end
