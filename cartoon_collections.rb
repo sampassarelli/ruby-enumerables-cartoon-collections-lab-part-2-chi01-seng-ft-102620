f3285a6a65e33cb8b86978e86b741d028f8898cd
Sam Passarelli <passarelli.sam@gmail.com>
2020-09-29 21:03:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |element|
-    if valid_calls.include?(element) 
+    if valid_calls == element
       element
     end
   end
